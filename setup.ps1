91ed848f32a0d1276cb9027e628f84da6bbeb6b0
Xyphuz <hansen0715.2@gmail.com>
2021-09-28 01:11:03 +0800

    v1

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,2 @@
-conda install selenium
-
-$source = 'https://chromedriver.storage.googleapis.com/93.0.4577.63/chromedriver_win32.zip'
-$filePath = './chromedriver.zip'
-Invoke-WebRequest -Uri $source -OutFile $filePath
-
-Expand-Archive -Path $filePath -DestinationPath './'
-
-Remove-Item $filePath
+pip install selenium
+pip install chromedriver-autoinstaller
